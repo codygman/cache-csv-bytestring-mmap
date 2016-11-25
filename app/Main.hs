d4b9c548ef257bc0820ec46ce2e168d385e58db1
Cody Goodman <codygman.consulting@gmail.com>
2016-11-24 20:54:38 -0600

    test using derive-storable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Lib
+import GHC.Generics
+import qualified Data.Vector.Storable.ByteString as BS
+import qualified Data.Vector.Storable as V
+import Foreign.Storable.Generic
+import GHC.Word
+import Data.Csv
+
+data Person = Person { name :: BS.ByteString
+                     , age :: Int
+                     } deriving (Show, Read, Generic)
+
+instance GStorable Person
+-- instance FromRecord Person
+-- instance ToRecord Person
+
 
 main :: IO ()
 main = someFunc
