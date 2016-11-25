89465301fadd951014a475d79edddf3140411f25
Cody Goodman <codygman.consulting@gmail.com>
2016-11-24 21:15:27 -0600

    failed attempt using generic-storable

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
-import GHC.Generics
 import qualified Data.Vector.Storable.ByteString as BS
+import qualified Data.Vector.Storable.ByteString.Char8 as C8
 import qualified Data.Vector.Storable as V
+
+import GHC.Generics
+import Foreign.Storable
 import Foreign.Storable.Generic
+
 import GHC.Word
 import Data.Csv
+import Data.Packed.Vector.MMap
 
 data Person = Person { name :: BS.ByteString
                      , age :: Int
                      } deriving (Show, Read, Generic)
 
-instance GStorable Person
--- instance FromRecord Person
--- instance ToRecord Person
-
-
 main :: IO ()
-main = someFunc
+main = do
+  let person = Person "Cody" 25
+  let people = V.fromList [person]
+  -- writeVector "person.mmap" people
+  print "ey"
